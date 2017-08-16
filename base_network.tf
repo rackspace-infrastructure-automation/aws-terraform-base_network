a5706479cf748506eb7d2128714ccda61664e484
Paul Williams <paul.williams@rackspace.co.uk>
2017-08-16 15:37:52 -0400

    Adding Paul's changes

--- base_network.tf
+++ base_network.tf
@@ -1,47 +1,61 @@
+/*
+  Base Network Module
+  Fanatical Support for Amazon Web Services
+  Rackspace
+*/
+
+## Variables
+
+# Descriptive name of the Environment to add to tags (should make sense to humans)
 variable "environment" {
   type = "string"
+  description = "The Environment this VPC is being deployed into (prod, dev, test, etc)"
 }
-
+# Name to give to the VPC and associated resources
 variable "name" {
   type = "string"
+  description = "The name of the VPC"
 }
-
+# Number of AZs to create
 variable "availability_zones_count" {
   default = "2"
   type = "string"
-  description = "Number of availability Zones to use"
+  description = "Number of Availability Zones to use"
 }
-
+# Instance Tenancy (can be dedicated or default)
 variable "instance_tenancy" {
   default = "default"
   type = "string"
-  description = "VPC instance tenancy (single tenant - dedicated, multi-tenancy - default"
+  description = "VPC Instance Tenancy (single tenant - dedicated, multi-tenancy - default)"
 }
-
+# The CIDR Range for the entire VPC
 variable "vpc_cidr_range" {
   default = "172.18.0.0/16"
   type = "string"
   description = "The IP Address space used for the VPC in CIDR notation."
 }
-
+# The CIDR Ranges for the Public Subnets
 variable "public_subnets" {
   type = "list"
-  description = "Public IP Subnets for AZ1-3."
+  description = "IP Address Ranges in CIDR Notation for Public Subnets in AZ1-3."
   default = ["172.18.0.0/22", "172.18.4.0/22", "172.18.8.0/22"]
 }
-
+# The CIDR Ranges for the Private Subnets
 variable "private_subnets" {
   type = "list"
   default = ["172.18.32.0/21", "172.18.40.0/21", "172.18.48.0/21"]
-  description = "Private IP Subnets for AZ 1-3."
+  description = "IP Address Ranges in CIDR Notation for Private Subnets in AZ 1-3."
+}
+# Number of VPN Gateways to create
+variable "vpn_gateways" {
+  default = "0"
+  description = "Number of VPN Gateways to provision"
 }
 
-variable "create_vpn" {
-  default = false
-  description = "Whether to provision a VPN Gateway."
-}
+## Resources
 
-resource "aws_vpc" "main" {
+### VPC
+resource "aws_vpc" "vpc" {
   cidr_block = "${var.vpc_cidr_range}"
   instance_tenancy = "${var.instance_tenancy}"
   enable_dns_support = "true"
@@ -49,57 +63,72 @@ resource "aws_vpc" "main" {
 
   tags {
     Environment = "${var.environment}"
-    Provisioner = "terraform"
-    Name = "${format("%s-vpc", var.name)}"
+    Provisioner = "rackspace"
+    Name = "${var.name}"
   }
 }
 
+### Internet Gateway
 resource "aws_internet_gateway" "internet" {
-  vpc_id = "${aws_vpc.main.id}"
+  vpc_id = "${aws_vpc.vpc.id}"
 
   tags {
     Environment = "${var.environment}"
-    Name = "${var.environment}-IGW"
-    Provisioner = "terraform"
+    Name = "${var.name}-IGW"
+    Provisioner = "rackspace"
   }
 }
 
+### Private Subnets
+# Loop over this as many times as necessary to create the correct number of Private Subnets
 resource "aws_subnet" "private_subnet" {
   count = "${var.availability_zones_count}"
-  vpc_id = "${aws_vpc.main.id}"
+  vpc_id = "${aws_vpc.vpc.id}"
   cidr_block = "${element(var.private_subnets, count.index)}"
 
   tags {
+    Environment = "${var.environment}"
     Name = "${format("%s-private-subnet-az%d", var.name, count.index + 1)}"
     Network = "private"
+    Provisioner = "rackspace"
   }
 }
 
+### Public Subnets
+# Loop over this as many times as necessary to create the correct number of Public Subnets
 resource "aws_subnet" "public_subnet" {
   count = "${var.availability_zones_count}"
-  vpc_id = "${aws_vpc.main.id}"
+  vpc_id = "${aws_vpc.vpc.id}"
   cidr_block = "${element(var.public_subnets, count.index)}"
 
   tags {
+    Environment = "${var.environment}"
     Name = "${format("%s-public-subnet-az%d", var.name, count.index + 1 )}"
     Network = "public"
+    Provisioner = "rackspace"
   }
 }
 
+### Elastic IPs
+# Need one per AZ for the NAT Gateways
 resource "aws_eip" "nat_gw_eip" {
   count = "${var.availability_zones_count}"
   vpc = true
 }
 
+### NAT Gateways
+# Loops as necessary to create one per AZ in the Public Subnets, and associate the provisioned Elastic IP
 resource "aws_nat_gateway" "nat" {
   allocation_id = "${element(aws_eip.nat_gw_eip.*.id, count.index)}"
   count = "${var.availability_zones_count}"
   subnet_id = "${element(aws_subnet.public_subnet.*.id, count.index)}"
 }
 
+### Private Subnet Route Tables
+# Routes traffic destined for `0.0.0.0/0` to the NAT Gateway in the same AZ
 resource "aws_route_table" "route_table_private" {
   count = "${var.availability_zones_count}"
-  vpc_id = "${aws_vpc.main.id}"
+  vpc_id = "${aws_vpc.vpc.id}"
 
   route {
     cidr_block = "0.0.0.0/0"
@@ -109,17 +138,21 @@ resource "aws_route_table" "route_table_private" {
   tags {
     Environment = "${var.environment}"
     Name = "${format("%s-PrivateRT-AZ%d", var.name, count.index +1)}"
+    Provisioner = "rackspace"
   }
 }
 
+### Private Subnet Route Table Associations
 resource "aws_route_table_association" "private_subnet_assocation" {
   count = "${var.availability_zones_count}"
   subnet_id = "${element(aws_subnet.private_subnet.*.id, count.index)}"
   route_table_id = "${element(aws_route_table.route_table_private.*.id, count.index)}"
 }
 
+### Public Route Tables
+# Routes traffic destined for `0.0.0.0/0` to the Internet Gateway for the VPC
 resource "aws_route_table" "route_table_public" {
-  vpc_id = "${aws_vpc.main.id}"
+  vpc_id = "${aws_vpc.vpc.id}"
 
   route {
     cidr_block = "0.0.0.0/0"
@@ -129,23 +162,29 @@ resource "aws_route_table" "route_table_public" {
   tags {
     Environment = "${var.environment}"
     Name = "${format("%s-PublicRT", var.name)}"
+    Provisioner = "rackspace"
   }
 }
 
+### Public Route Table Associations
 resource "aws_route_table_association" "public_subnet_assocation" {
   count = "${var.availability_zones_count}"
   subnet_id = "${element(aws_subnet.public_subnet.*.id, count.index)}"
   route_table_id = "${aws_route_table.route_table_public.id}"
 }
 
+### VPN Gateways
 resource "aws_vpn_gateway" "vpn" {
   count = "${var.create_vpn}"
-  vpc_id = "${aws_vpc.main.id}"
+  vpc_id = "${aws_vpc.vpc.id}"
 
   tags {
     Name = "${format("%s-VPNGateway", var.name)}"
+    Provisioner = "rackspace"
   }
 }
+
+## Outputs
 
 output "private_subnets" {
   value = [ "${aws_subnet.private_subnet.*.id}" ]
@@ -156,5 +195,5 @@ output "public_subnets" {
 }
 
 output "vpc_id" {
-  value = "${aws_vpc.main.id}"
+  value = "${aws_vpc.vpc.id}"
 }
